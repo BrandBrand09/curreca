b78a042164feff3f051097899f006f1ea0ca1397
GabrielRechBrand <rcgabrielbrand@gmail.com>
2025-02-11 17:53:00 -0300

    style: Identation fix.

--- src/main/java/item/CurrecaItems.java
+++ src/main/java/item/CurrecaItems.java
@@ -8,15 +8,12 @@ import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
 public class CurrecaItems {
+
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, CurrecaMod.MODID);
 
-    public static final RegistryObject<Item> BLANKRUNE = ITEMS.register("blankrune",
-            () -> new Item(new Item.Properties()
-                    .setId(ITEMS.key("blankrune"))
-            ));
-
-
-
+    public static final RegistryObject<Item> BLANKRUNE = ITEMS.register("blankrune", () -> {
+        return new Item(new Item.Properties().setId(ITEMS.key("blankrune")));
+    });
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
